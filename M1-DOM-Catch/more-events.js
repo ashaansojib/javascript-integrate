68a51674b4ea922144a948b20999437c545d0729
ashaansojib <ashaduzzamansojib67@gmail.com>
2023-02-11 10:24:57 +0600

    add delegate

--- M1-DOM-Catch/more-events.js
+++ M1-DOM-Catch/more-events.js
@@ -34,3 +34,28 @@ document.getElementById("delete-btn").addEventListener('click', function(){
     const secret = document.getElementById("triggered");
     secret.style.display = "none";
 })
+// --------------------------javascript buble-----------------------------------------
+// stopPropagation or immidiatePropagation: kno ekta event ke specific vabe use kore hole stopPropagation function add korte hbe. ata muloto upor theke capture kore and call korar smy nich thke upre jay
+document.getElementById("second-item").addEventListener('click', function(event){
+    console.log("this is a item");
+    event.stopPropagation();
+})
+document.getElementById("list-container").addEventListener('click', function(event){
+    console.log("this is list container")
+    event.stopImmediatePropagation();
+})
+document.getElementById("prop").addEventListener('click', function(event){
+    console.log("this is article container")
+    event.stopImmediatePropagation();
+})
+
+// -------------------------------------javascript delegate---------------------------------------
+document.getElementById("delegate-container").addEventListener('click', function(event){
+    event.target.parentNode.removeChild(event.target);
+})
+document.getElementById("delegated").addEventListener('click', function(){
+    const li = document.createElement("li");
+    li.innerText = "added new child item";
+    const delegateParent = document.getElementById("delegate-container");
+    delegateParent.appendChild(li);
+})
